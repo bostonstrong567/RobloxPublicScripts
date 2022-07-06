7020bd569afc32a399d4f2c74a94dd9f09b51c12
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2022-07-06 12:20:27 -0400

    Update BidBattlesFunctions.lua

--- BidBattlesFunctions.lua
+++ BidBattlesFunctions.lua
@@ -7,7 +7,7 @@ git.ItemName = Instance.new("TextLabel")
 git.ItemCorner = Instance.new("UICorner")
 
 git.FloatItemValue.Name = "FloatItemValue"
-git.FloatItemValue.Parent = game:GetService("Workspace")
+git.FloatItemValue.Parent = nil
 git.FloatItemValue.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 git.FloatItemValue.Active = true
 git.FloatItemValue.AlwaysOnTop = true
@@ -16,13 +16,13 @@ git.FloatItemValue.LightInfluence = 1.000
 git.FloatItemValue.Size = UDim2.new(4.19999981, 0, 2.29999995, 0)
 
 git.ItemFrame.Name = "ItemFrame"
-git.ItemFrame.Parent = FloatItemValue
+git.ItemFrame.Parent = git.FloatItemValue
 git.ItemFrame.BackgroundColor3 = Color3.fromRGB(14, 0, 62)
 git.ItemFrame.Size = UDim2.new(1, 0, 1, 0)
 git.ItemFrame.BackgroundTransparency = 1
 
 git.ItemName.Name = "ItemName"
-git.ItemName.Parent = ItemFrame
+git.ItemName.Parent = git.ItemFrame
 git.ItemName.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 git.ItemName.BackgroundTransparency = 1.000
 git.ItemName.Position = UDim2.new(0.150000006, 0, 0.150000006, 0)
@@ -36,6 +36,6 @@ git.ItemName.TextWrapped = true
 
 git.ItemCorner.CornerRadius = UDim.new(0, 4)
 git.ItemCorner.Name = "ItemCorner"
-git.ItemCorner.Parent = ItemFrame
+git.ItemCorner.Parent = git.ItemFrame
 
 return git
